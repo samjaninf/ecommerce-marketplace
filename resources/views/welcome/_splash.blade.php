d9ba60b3d87974c346890dd554ab940b727e463e
matt_fay <mattfay@live.com>
2015-12-16 11:39:57 +0000

    <

--- resources/views/welcome/_splash.blade.php
+++ resources/views/welcome/_splash.blade.php
@@ -2,7 +2,6 @@
   <video loop="loop"  autoplay="autoplay" poster="/video/splash.jpg" title="KoolBeans">
     <source src="/video/splash.m4v" type="video/mp4" />
     <source src="/video/splash.webm" type="video/webm" />
-    <source src="/video/splash.mp4" />
   </video>
     <div id="splash-contents" class="container-fluid">
         <h1>Order great coffee,<br>from great coffee shops</h1>
