4cc74019acbfc31a5218f9d278d046f78d002499
Matt Gould <mattg12323@bitbucket.org>
2016-01-20 11:52:37 +0000

    add db cols to coffee_shops

--- database/migrations/2016_01_14_142237_add_stripe_to_coffee_shops.php
+++ database/migrations/2016_01_14_142237_add_stripe_to_coffee_shops.php
@@ -17,7 +17,6 @@ class AddStripeToCoffeeShops extends Migration
             $table->string('stripe_publishable_key');
             $table->string('stripe_scope');
             $table->string('stripe_livemode');
-            $table->string('stripe_user_id');
             $table->string('stripe_refresh_token');
             $table->string('stripe_access_token');
         });
@@ -35,7 +34,6 @@ class AddStripeToCoffeeShops extends Migration
             $table->dropColumn('stripe_publishable_key');
             $table->dropColumn('stripe_scope');
             $table->dropColumn('stripe_livemode');
-            $table->dropColumn('stripe_user_id');
             $table->dropColumn('stripe_refresh_token');
             $table->dropColumn('stripe_access_token');
         });
