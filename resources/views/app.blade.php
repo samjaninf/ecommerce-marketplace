aeef5d421e1dd686f45e2d5eeaa249048fcbda97
Matt Gould <mattg12323@bitbucket.org>
2016-01-29 12:00:36 +0000

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,7 +21,7 @@
     <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-</head>
+</head> 
 <body>
 <div id="dtBox"></div>
 <header id="top-container" @if(app('request')->is('/')) style="height: 550px"@endif>
