c0a898b1d82e9b9df9baabf5fd6da6f84b26dca8
Matt Gould <mattg12323@bitbucket.org>
2016-02-24 16:45:28 +0000

    opps to oops

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,14 +9,14 @@
 
         <div class="row">
             <div class="col-xs-12">
-                <h1>Opps, an error occured.</h1>
+                <h1>Oops, an error occured.</h1>
             </div>
         </div>
 
         <div class="row">
             <div class="col-xs-12">
                 <p>
-                    Opps, an error occured. We've been notified and will take a look. In the meantime why not search for a coffee shop?<br>
+                    Oops, an error occured. We've been notified and will take a look. In the meantime why not search for a coffee shop?<br>
                     <br>
 
                     <a href="{{ URL::previous() }}" class="btn btn-primary">Go Back</a>
